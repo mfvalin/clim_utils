31fa812ad955f3438b1451d768bccfc5c7554c10
Michel Valin <mfvalin@gmail.com>
2018-03-29 17:23:42 -0400

    activated installer

--- bin/install_clim_utils.sh
+++ bin/install_clim_utils.sh
@@ -4,3 +4,9 @@ Where=${Where%/bin/*}/src
 cd ${Where}
 echo "Installing from ${Where}"
 [[ "$BASE_ARCH" == "$EC_ARCH" ]] && echo "ERROR: EC_ARCH == BASE_ARCH" && exit 1
+set -x
+for Target in *.F90 ; do
+  s.f90 -I. -O 2 -o ${Target%.F90}.Abs ${Target} -lrmn
+  mv ${Target%.F90}.Abs ../bin
+  rm -f *.mod
+done
